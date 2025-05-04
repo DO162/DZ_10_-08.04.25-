d9a5fd92919907d9aa9a367b0dca86437140dd85
DO162 <ds9893814@gmail.com>
2025-05-05 01:09:42 +0300

    Add files via upload

---
+++ DZ_10_(08.04.25)/DZ_10_(08.04.25).cpp
@@ -0,0 +1,48 @@
+﻿#include "Header.h"
+
+int main() {
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	srand(time(0));
+	rand();
+	system("title Bomberman");
+
+	// cursor hide
+	CURSOR_HIDE(h);
+
+	const int HEIGHT = 25;
+	const int WIDTH = 65;
+	int map[HEIGHT][WIDTH] = {};
+	// ASCII Table
+	//for (int i = 0; i < 256; i++)
+	//{
+	//	cout << (char)i << " - " << i << "\n";
+	//}
+
+	// MODEL (логіка заповнення масиву)
+
+	MODEL(HEIGHT, WIDTH, map);
+
+	///////////////////////////////////////////////////////////
+
+	// VIEW (подання - як буде виглядати ігрова локація)
+	VIEW(HEIGHT, WIDTH, map, h);
+
+	////////////////////////////////////////////////////////////
+	// показ ГГ
+
+	COORD hero;
+	hero.X = 0;
+	hero.Y = 2;
+	static int coins_collected = 0;
+	COORD coins_info;
+	coins_info.X = WIDTH + 1;
+	coins_info.Y = 0;
+
+	Main_Character(WIDTH, h, hero, coins_info, &coins_collected);
+
+	// ігровий двигунчик
+	Game_Engine(map, hero, h, &coins_collected, WIDTH);
+
+
+	Sleep(INFINITE);
+}
